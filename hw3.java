ea80bac45e8e9ce8f3b08d8b7c25513a62c8d263
Alicia Ning <alicia.ning@gmail.com>
2016-02-08 17:04:40 -0800

    read function added

--- hw3.java
+++ hw3.java
@@ -5,8 +5,48 @@
  * 11 February 2016
  * --------------------------------------------------------------------------*/
 
+import java.io.*;
+import java.util.*;
+import java.text.*;
+
 public class hw3 {
+	/* function to read files */
+	public static LinkedList<Integer[]> read (File file) {
+		LinkedList<Integer[]> res = new LinkedList<Integer[]>();
+		String line = null;
+
+		try {
+			FileReader fReader = new FileReader(file);
+			BufferedReader bReader = new BufferedReader(fReader);
+
+			/* while there are still lines to read */
+			while((line = bReader.readLine()) != null) {
+				/* split each line by spaces */
+				String[] vals = line.split(" ");
+				/* array to store values */
+				Integer[] intVals = new Integer[vals.length];
+
+				/* store values as integers */
+				for(int i = 0; i < intVals.length; i++) {
+					intVals[i] = Integer.parseInt(vals[i]);
+				}
+
+				/* add to LL */
+				res.add(intVals);
+			}
+
+			/* close file */
+			bReader.close();
+		} catch(FileNotFoundException ex) {
+			System.out.println("Failed to open file");
+		} catch(IOException ex) {
+			System.out.println("Error reading file");
+		}
+
+		return res;
+	}
+
 	public static void main (String[] args) {
-		
+
 	}
 }
